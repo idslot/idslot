cd50ea904d83d05b60e318029499d7e9853843b3
Armin Borumand <armin390@gmail.com>
2012-05-25 17:42:39 +0430

    Add version to config

--- config/config.php
+++ config/config.php
@@ -390,6 +390,9 @@ $config['rewrite_short_tags'] = FALSE;
 */
 $config['proxy_ips'] = '';
 
+// Version
+$config['version'] = '1.0';
+
 // Plugins
 $config['plugins'] = array('about'=>'About', 'photos'=>'Photos', 'links'=>'Links', 'contact'=>'Contact');
 
